fb5bd2bf3eb4379f3d200ac85a8d7d6affe062a9
SkyMinus <minus_rav@qq.com>
2026-01-04 16:18:26 +0800

    nuwa: Update blobs from OS3.0.4.0.WMBCNXM
    Change-Id: I6d38b0eed5d781d5ba7bd4c828cfa304cc7b391a

--- lineage_nuwa.mk
+++ lineage_nuwa.mk
@@ -26,4 +26,4 @@ PRODUCT_MODEL := 2210132C
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Xiaomi/nuwa/nuwa:15/AQ3A.240912.001/OS2.0.212.0.VMBCNXM:user/release-keys
+    BuildFingerprint=Xiaomi/nuwa/nuwa:16/BP2A.250605.031.A3/OS3.0.3.0.WMBCNXM:user/release-keys
